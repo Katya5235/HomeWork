f15b30892d13d4b432cabfa98f9496443bcd8d9c
Katya5235 <125498384+Katya5235@users.noreply.github.com>
2023-05-20 15:31:32 +0300

    sum of digits in a three-digit number

--- sem1/task2.py
+++ sem1/task2.py
@@ -2,4 +2,16 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
+print (' Введите число: ')
+n = int (input())
+sum_of_digits = 0
+
+while n > 0:
+    digit = n % 10
+    n = n // 10
+    sum_of_digits = sum_of_digits + digit 
+
+print (sum_of_digits) 
+
+
 
