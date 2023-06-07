7f12c19390d6e624c0ae2933acc0240a61091305
Katya5235 <125498384+Katya5235@users.noreply.github.com>
2023-06-07 13:42:13 +0300

    ягоды

---
+++ sem4/task24.py
@@ -0,0 +1,35 @@
+# Задача 24: В фермерском хозяйстве в Карелии выращивают чернику.
+# Она растёт на круглой грядке, причём кусты высажены только по окружности.
+# Таким образом, у каждого куста есть ровно два соседних. 
+# Всего на грядке растёт N кустов.
+# Эти кусты обладают разной урожайностью, 
+# поэтому ко времени сбора на них выросло различное число ягод — на i-ом кусте выросло ai ягод.
+# В этом фермерском хозяйстве внедрена система автоматического сбора черники.
+# Эта система состоит из управляющего модуля и нескольких собирающих модулей.
+# Собирающий модуль за один заход, находясь непосредственно перед некоторым кустом,
+# собирает ягоды с этого куста и с двух соседних с ним.
+# Напишите программу для нахождения максимального числа ягод,
+# которое может собрать за один заход собирающий модуль, 
+# находясь перед некоторым кустом заданной во входном файле грядки.
+
+def generate_list_of_counts_of_berries(n: int, a: float) -> list[int]:
+    return
+
+
+number_of_bushes = int(input('Введите количество кустов: '))  
+yield_factor = float(input('Введите коэффициент урожайности: '))  
+list_of_counts_of_berries = [int(yield_factor * i) for i in range(number_of_bushes)] 
+maximum_count_of_berries = 0
+index_of_middle_element_giving_maximum_summ_of_berries = 0
+for i in range(len(list_of_counts_of_berries)):
+    count_of_berries_on_left = list_of_counts_of_berries[i]  
+    count_of_berries_in_the_middle = list_of_counts_of_berries[(i + 1) % len(list_of_counts_of_berries)]  
+    count_of_berries_on_right = list_of_counts_of_berries[(i + 2) % len(list_of_counts_of_berries)]  
+
+    current_count_of_berries = count_of_berries_on_left + count_of_berries_in_the_middle + count_of_berries_on_right
+    if current_count_of_berries > maximum_count_of_berries:
+        maximum_count_of_berries = current_count_of_berries
+        index_of_middle_element_giving_maximum_summ_of_berries = count_of_berries_on_right
+print(f'Список с ягодами: {list_of_counts_of_berries}')
+print(
+    f'Максимальное число ягод для сбора за один заход: {maximum_count_of_berries}\n индекс среднего куста: {index_of_middle_element_giving_maximum_summ_of_berries}')
