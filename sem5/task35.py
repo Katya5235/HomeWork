93b9eb3721869e6e19d8dad29d9f66dd977661d2
Katya5235 <125498384+Katya5235@users.noreply.github.com>
2023-06-07 19:38:05 +0300

    chacge

--- sem5/task35.py
+++ sem5/task35.py
@@ -8,11 +8,15 @@
 
 prime_number = int(input("Введите число: "))
 counter = 0
-for i in range (2, prime_number // 2+1):
+for i in range (2, prime_number ):
     if (prime_number % i == 0 ):
         counter = counter +1 
-
+        
 if (counter <= 0 ):
     print ("Число простое! ")
 else:
     print("Число непростое! ")     
+
+
+
+
